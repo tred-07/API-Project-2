5a2db0da8739b82aec99dae291e56fd1ae66a148
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-11-04 17:30:38 +0600

    upd by dan-sg

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # API-Project-2
+# For resources use JSONPlaceholder
 
+```txt
+CRUD Operation:
+C for Create, R for Read, U for Update, D for Delete
 ```
+
+# 
+```txt
 Press F12 or go to inspection mode.
 Network tab. Reload you see some thing.
 Generally, you see 1st row name:post.html status:304 
--- posts.js
+++ posts.js
@@ -1,14 +1,52 @@
-const posts=()=>{
-    fetch("https://jsonplaceholder.typicode.com/posts").then(res=>res.json()).then(data=>showPosts(data))
-}
+const posts = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => showPosts(data));
+};
 
-const showPosts=(data)=>{
-    for(const it of data){
-        console.log(it)
-        let div=document.createElement("div")
-        div.innerHTML=`<div style="display:flex;border:2px solid purple;margin:10px;padding:10px;">
+const showPosts = (data) => {
+  for (const it of data) {
+    console.log(it);
+    let div = document.createElement("div");
+    div.innerHTML = `<div style="display:flex;border:2px solid purple;margin:10px;padding:10px;">
         <p style="font-size:2rem">User id: ${it.userId},<br> id: ${it.id}, title: ${it.title}, <br> Description: ${it.body}</p></div>
-        `
-        document.getElementById("posts").appendChild(div)
-    }
-}
+        `;
+    document.getElementById("posts").appendChild(div);
+  }
+};
+
+const deletePost = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+};
+
+const patchPost = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
+
+const createApost = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
